0fcbe61d30acd4231c63b03f9170c45710739f2d
Konstantin Kruts <kkruts@yahoo.com>
2019-02-25 22:37:04 +0800

    smth new

---
+++ masscan_belkin_search.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# the parametr should be a filename with ip ranges to scan
+
+iptables -A INPUT -p tcp --dport 61111 -j DROP
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    # echo "IP read from file: $line"
+    masscan $line -p80,443 --banners --source-port 61111 -oG mass_raw_res.txt --max-rate 10000
+    grep -f banners.txt mass_raw_res.txt | grep -o -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | sort | uniq > ${line////-}.txt
+done < "$1"
