6b2497f89a54c629881b0eb51f5c41e15d8744d4
Konstantin Kruts <kkruts@yahoo.com>
2019-02-26 18:22:24 +0800

    smth new

--- ftpupload.py
+++ ftpupload.py
@@ -10,7 +10,7 @@ passWD = 'Dap8K7shQbIx28q'
 fileToUpload = sys.argv[1]
 
 ftp = FTP('217.182.200.111', userName, passWD)
-print(ftp.dir())
+# print(ftp.dir())
 ftp.cwd('mass_result')
 f = open(fileToUpload, 'r')
 ftp.storlines("STOR "+ fileToUpload, f)
