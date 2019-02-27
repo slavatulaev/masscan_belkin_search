a46676908fc729b441050ba8d02f4354d49b938b
Konstantin Kruts <kkruts@yahoo.com>
2019-02-27 19:03:47 +0800

    smth new

--- masscan_belkin_search.sh
+++ masscan_belkin_search.sh
@@ -4,8 +4,12 @@
 iptables -A INPUT -p tcp --dport 61112 -j DROP
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "IP read from file: $line"
-    outFile=${line////-}.txt
+    echo "Current IP range: $line"
+    # remove leading whitespace characters
+    trimmedLine="${line#"${line%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    trimmedLine="${trimmedLine%"${trimmedLine##*[![:space:]]}"}"   
+    outFile=${trimmedLine////-}.txt
     echo "Output to file $outFile"
     masscan $line -p80,443 --banners --source-port 61112 -oG mass_raw_res.txt --max-rate 10000
     grep -f banners.txt mass_raw_res.txt | grep -o -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | sort | uniq > $outFile
