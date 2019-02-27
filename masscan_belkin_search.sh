09304f8ef91efcc4bfd70ea21ef3d6fac73d2002
Konstantin Kruts <kkruts@yahoo.com>
2019-02-27 18:50:33 +0800

    smth new

--- masscan_belkin_search.sh
+++ masscan_belkin_search.sh
@@ -7,7 +7,9 @@ password=''
 iptables -A INPUT -p tcp --dport 61111 -j DROP
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    # echo "IP read from file: $line"
+    echo "IP read from file: $line"
+    outFile=${line////-}.txt
+    echo "Output to file $outFile"
     masscan $line -p80,443 --banners --source-port 61111 -oG mass_raw_res.txt --max-rate 10000
     grep -f banners.txt mass_raw_res.txt | grep -o -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | sort | uniq > ${line////-}.txt
     ./ftpupload.py ${line////-}.txt
