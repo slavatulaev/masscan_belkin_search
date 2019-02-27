f24af6868d529bda4a0a9806d4ea6377e93f7d8c
Konstantin Kruts <kkruts@yahoo.com>
2019-02-27 18:56:52 +0800

    smth new

--- masscan_belkin_search.sh
+++ masscan_belkin_search.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 # the parametr should be a filename with ip ranges to scan
-IP_address=''
-username=''
-password=''
 
-iptables -A INPUT -p tcp --dport 61111 -j DROP
+iptables -A INPUT -p tcp --dport 61112 -j DROP
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "IP read from file: $line"
     outFile=${line////-}.txt
     echo "Output to file $outFile"
-    masscan $line -p80,443 --banners --source-port 61111 -oG mass_raw_res.txt --max-rate 10000
-    grep -f banners.txt mass_raw_res.txt | grep -o -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | sort | uniq > ${line////-}.txt
-    ./ftpupload.py ${line////-}.txt
-    rm ${line////-}.txt
+    masscan $line -p80,443 --banners --source-port 61112 -oG mass_raw_res.txt --max-rate 10000
+    grep -f banners.txt mass_raw_res.txt | grep -o -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | sort | uniq > $outFile
+    ./ftpupload.py $outFile
+    rm $outFile
 
 done < "$1"
