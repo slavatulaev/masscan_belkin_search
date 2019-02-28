14d083feb7faeeb5264ed738e66e3debe985a4a7
Konstantin Kruts <kkruts@yahoo.com>
2019-02-28 20:14:24 +0800

    smth new

--- ftpupload.py
+++ ftpupload.py
@@ -8,10 +8,18 @@ serverAddr = '217.182.200.111'
 userName = 'user543116152'
 passWD = 'Dap8K7shQbIx28q'
 fileToUpload = sys.argv[1]
-
+print("opening ftp...")
 ftp = FTP('217.182.200.111', userName, passWD)
-# print(ftp.dir())
+print("ftp.dir()")
+print("changing directory...")
 ftp.cwd('mass_result')
-f = open(fileToUpload, 'r')
-ftp.storlines("STOR "+ fileToUpload, f)
-ftp.quit()
+print("directory changed succesfully...")
+try:
+    print("opening file " + fileToUpload)
+    f = open(fileToUpload, 'r')
+    print("sending file to ftp...")
+    ftp.storlines("STOR "+ fileToUpload, f)
+    print("closing ftp connection ...")
+    ftp.quit()
+except:
+    print("faled to upload %s to ftp" % fileToUpload)
