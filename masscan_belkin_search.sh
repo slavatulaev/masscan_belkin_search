4ffffa5e6bbe62dc2b0de2e66a6982c41dde7857
Konstantin Kruts <kkruts@yahoo.com>
2019-02-28 18:27:06 +0800

    smth new

--- masscan_belkin_search.sh
+++ masscan_belkin_search.sh
@@ -12,8 +12,12 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     outFile=${trimmedLine////-}.txt
     echo "Output to file $outFile"
     masscan $line -p80,443 --banners --source-port 61112 -oG mass_raw_res.txt --max-rate 10000
+    echo "Finished scanning... now performing grep..."
     grep -f banners.txt mass_raw_res.txt | grep -o -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | sort | uniq > $outFile
+    echo "grep IPs to $outFile done. Now sendint it to FTP..."
     ./ftpupload.py $outFile
+    echo "File $outFile uploaded to ftp server. Time to delete it..."
     rm $outFile
-
+    echo "File $outFile sucsessfully deleted"
+    echo "End of current cycle. Going Next..."
 done < "$1"
